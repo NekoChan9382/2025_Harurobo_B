cef48b73c75e4241e6507a0f746e7f560642095b
Neko-Linux <mushbeen25@gmail.com>
2025-02-26 18:57:58 +0900

    refactor: 不要な処理、変数の削除

--- src/main.cpp
+++ src/main.cpp
@@ -14,20 +14,9 @@ int main()
     constexpr int pid_max = 1;
     constexpr int dji_max_output = 8000;
     constexpr int motor_amount = 4;
-    int velocity_xy[2] = {0}; // x, y, ang
-    float velocity_ang = 0;
+
     int pos_mm[3] = {0}; // x, y, ang
     int motor_velocity[motor_amount] = {0};
-
-    constexpr int wheel_radius = 34;     // mm
-    constexpr int robot_size_calc = 190; // mm
-    constexpr int robot_size_motor = 240;
-
-    constexpr int max_dps_trans = 20000;
-    constexpr int max_dps_rot = 500;
-
-    int encoder_data[motor_amount] = {0};
-    int encoder_data_pre[motor_amount] = {0};
 
     CAN can(PA_11, PA_12, 1000000);
     CANMessage msg_encoder;
@@ -65,35 +54,12 @@ int main()
             {
                 encoder_data[i] = msg_encoder.data[2 * i + 1] << 8 | msg_encoder.data[2 * i];
             }
-            printf("enc_data\n%d\n%d\n%d\n%d\n", encoder_data[0], encoder_data[1], encoder_data[2], encoder_data[3]);
+            printf("enc_data,%d,%d,%d,%d\n", encoder_data[0], encoder_data[1], encoder_data[2], encoder_data[3]);
         }
 
         char data[10] = "";
         if (readline(pc, data) == 0)
         {
-            // printf("%s\n", data);
-
-            if (strcmp(data, "trans") == 0)
-            {
-                // printf("trans!!\n");
-                for (int i = 0; i < 2; i++)
-                {
-                    char data_trans[10] = "";
-                    if (readline(pc, data_trans, false, true) == 0)
-                    {
-                        velocity_xy[i] = atof(data_trans) * max_dps_trans * -1;
-                    }
-                }
-            }
-            if (strcmp(data, "rot") == 0)
-            {
-                char data_rot[10] = "";
-                if (readline(pc, data_rot, false, true) == 0)
-                {
-                    velocity_ang = atof(data_rot) * max_dps_rot * -1;
-                    // printf("velocity_ang: %f\n", velocity_ang);
-                }
-            }
             if (strcmp(data, "vel") == 0)
             {
                 for (int i = 0; i < motor_amount; i++)
@@ -119,10 +85,9 @@ int main()
                 float motor_dps = c620.get_rpm(i + 1) * rmp_to_rad;
                 int goal_ang_vel = motor_velocity[i];
                 const float percent = pid[i].calc(goal_ang_vel, motor_dps, elapsed);
-                const int out = percent * dji_max_output;
-                printf("dps: %f, goal: %d, out: %d\n", motor_dps, goal_ang_vel, out);
+                // printf("dps: %f, goal: %d, out: %d\n", motor_dps, goal_ang_vel, out);
 
-                c620.set_output(out, i + 1);
+                c620.set_output_percent(percent, i + 1);
             }
 
             for (int i = 0; i < motor_amount; i++)
